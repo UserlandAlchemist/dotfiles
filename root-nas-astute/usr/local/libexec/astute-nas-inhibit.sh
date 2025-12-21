c8946c677ac73fd3bbe156f2217111d4efdd5215
Userland Alchemist <alchemist@userlandlab.org>
2025-12-21 17:03:12 +0000

    move nas-inhibit servcies to user scope

--- root-nas-astute/usr/local/libexec/astute-nas-inhibit.sh
+++ root-nas-astute/usr/local/libexec/astute-nas-inhibit.sh
@@ -3,10 +3,10 @@ set -eu
 
 case "${SSH_ORIGINAL_COMMAND:-}" in
   start)
-    systemctl start nas-inhibit.service
+    systemctl --user start nas-inhibit.service
     ;;
   stop)
-    systemctl stop nas-inhibit.service
+    systemctl --user stop nas-inhibit.service
     ;;
   *)
     logger -t astute-nas-inhibit "Rejected command: ${SSH_ORIGINAL_COMMAND:-<none>}"
