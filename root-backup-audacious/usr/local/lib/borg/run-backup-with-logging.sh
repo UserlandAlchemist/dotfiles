0d7d93b78a6ee7674b24e1adc83afac8139c252b
Userland Alchemist <alchemist@userlandlab.org>
2025-12-24 23:09:07 +0000

    audacious: send borg output to journald

---
+++ root-backup-audacious/usr/local/lib/borg/run-backup-with-logging.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+
+# Log all output directly to the journal.
+{
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting backup service"
+    /usr/local/lib/borg/run-backup.sh
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] Backup service completed"
+} 2>&1 | systemd-cat -t borg-backup
