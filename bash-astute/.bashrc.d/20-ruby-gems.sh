63d578b1b26e2d49ce6b012fe34d292049a95a20
Userland Alchemist <alchemist@userlandlab.org>
2026-01-14 16:14:19 +0000

    bash-astute: add user gem PATH
    Expose user gem executables like mdl on astute.

---
+++ bash-astute/.bashrc.d/20-ruby-gems.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Add user gem executables to PATH when present.
+
+gems_bin="$HOME/.local/share/gem/ruby/3.3.0/bin"
+if [ -d "$gems_bin" ]; then
+	case ":$PATH:" in
+		*":$gems_bin:"*)
+			;;
+		*)
+			export PATH="$gems_bin:$PATH"
+			;;
+	esac
+fi
